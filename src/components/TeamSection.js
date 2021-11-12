f78a24495c6ebd48a9f6ef0881c881b92835d439
Stackbit Code Editor <projects@stackbit.com>
2021-11-12 20:50:48 +0000

    TeamSection.js: changed

--- src/components/TeamSection.js
+++ src/components/TeamSection.js
@@ -15,7 +15,7 @@ export default class TeamSection extends React.Component {
         const lastName = _.get(teamMember, 'last_name', '');
         const name = _.trim(`${firstName} ${lastName}`);
         const bio = _.get(teamMember, 'bio');
-        const age = _.get()
+        const age = _.get(age)
         return (
             <div key={index} className="cell">
                 <div className="card team-member">
